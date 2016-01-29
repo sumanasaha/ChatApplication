437ac932c7554a91f234c546e2055f756c99f402
Sumana Saha <sumanasaha@Tejas-Mac-mini.local>
2016-01-29 17:44:42 +0530

    Layout changed, time function, border to the bottom

--- Chatapplication/app/src/main/java/com/example/sumanasaha/chatapplication/VivzAdapter.java
+++ Chatapplication/app/src/main/java/com/example/sumanasaha/chatapplication/VivzAdapter.java
@@ -26,13 +26,19 @@ import java.util.List;
 public class VivzAdapter extends RecyclerView.Adapter<VivzAdapter.MyViewHolder>{
     private LayoutInflater inflater;
     ArrayList<String> data= new ArrayList<>();
-    int height;
+    int height,hl,ml;
+    String currentTimeString;
 
-    public VivzAdapter(Context context,ArrayList<String> data,int height)
+
+
+    public VivzAdapter(Context context,ArrayList<String> data,int height,int hl,String currentTimeString)
     {
         inflater = LayoutInflater.from(context);
         this.data=data;
         this.height=height;
+        this.hl=hl;
+        this.ml=ml;
+        this.currentTimeString=currentTimeString;
     }
 
     public VivzAdapter() {
@@ -40,27 +46,29 @@ public class VivzAdapter extends RecyclerView.Adapter<VivzAdapter.MyViewHolder>{
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-       View view= inflater.inflate(R.layout.custom_row,parent,false);
+       View view= inflater.inflate(R.layout.custom_row, parent, false);
         MyViewHolder holder=new MyViewHolder(view);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        if(position==0)
+        if(data.size()==1)
         {
-           // int proportionalHeight = containerHeight((MainActivity) mCntx);
 
 
 
+          int holderheight=holder.container.getHeight();
             TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT); // (width, height)
-
-            params.topMargin= (int) (height*0.7);
+           // params.topMargin=height-(hl+holderheight);
+            int margin= (int) (height*0.5);
+            params.topMargin= (int) margin;
             holder.container.setLayoutParams(params);
         }
 
         if(data.size()!=0) {
             holder.title.setText(data.get(position));
+            holder.date.setText(currentTimeString);
           //  holder.icon.setImageResource(current.iconId);
         }
 
@@ -73,15 +81,16 @@ public class VivzAdapter extends RecyclerView.Adapter<VivzAdapter.MyViewHolder>{
 
     class MyViewHolder extends RecyclerView.ViewHolder{
         LinearLayout container;
-
+        LinearLayout bottomsection1;
         TextView title;
+        TextView date;
        // ImageView icon;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             title= (TextView) itemView.findViewById(R.id.listText);
             container = (LinearLayout) itemView.findViewById(R.id.containerlayout);
-
+            date= (TextView) itemView.findViewById(R.id.date);
 
        //     icon= (ImageView) itemView.findViewById(R.id.listIcon);
         }
